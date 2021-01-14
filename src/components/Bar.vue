88b7b5b2f705dbb9a124c4d75e5d356e49dc17c8
sheldon <sotyoyotao@gmail.com>
2021-01-14 11:59:28 +0800

    first init

---
+++ src/components/Bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello Bar!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Bar',
+}
+</script>
+<style scoped="true">
+h1 {
+  color: aquamarine;
+}
+</style>
---
+++ src/components/Foo.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello Foo!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Foo',
+}
+</script>
+<style scoped="true">
+h1 {
+  color: aqua;
+}
+</style>
