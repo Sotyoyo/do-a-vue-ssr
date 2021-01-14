2fe38b49cc8c629410d101e7eb28b7163ab17337
sheldon <sotyoyotao@gmail.com>
2021-01-14 14:43:18 +0800

    split configs

---
+++ configs/webpack.base.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '../src/client.js'),
+  module: {
+    rules: [
+      { test: /\.vue$/, use: 'vue-loader' },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader', // @babel/core -> preset-env
+          options: {
+            presets: ['@babel/preset-env'], // 插件的集合
+          },
+        },
+        exclude: /node_modules/, // 表示node_modules的下的文件不需要查找
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false, // 注意为了配套使用vue-style-loader
+            },
+          },
+        ], // 从右向左执行
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+}
--- configs/webpack.server.js
+++ configs/webpack.server.js
@@ -5,10 +5,10 @@ const baseConfig = require('./webpack.base')
 
 module.exports = merge(baseConfig, {
   mode: 'development',
-  entry: path.resolve(__dirname, '../src/client.js'),
+  entry: path.resolve(__dirname, '../src/server.js'),
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
+      template: path.resolve(__dirname, '../public/template.html'),
     }),
   ],
 })
